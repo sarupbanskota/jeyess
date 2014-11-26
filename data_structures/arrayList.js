f1e7e8c92290da07ab99ff0e70e9b11e191b3514
Sarup Banskota <sbanskota08@gmail.com>
2014-11-26 18:42:36 +0530

    Provide a list -> array function

--- data_structures/arrayList.js
+++ data_structures/arrayList.js
@@ -8,5 +8,15 @@ var arrayToList = function(array){
 	return list;
 }
 
+var listToArray = function(list){
+	array = [];
+	array.push(list.value);
+	if(list.rest!= null){
+		array = array.concat(listToArray(list.rest));
+	}
+	return array;
+}
+
 console.log(arrayToList([10, 20,30]));
+console.log(listToArray({ value: 10, rest: { value: 20, rest: { value: 30, rest: null }}}));
 
