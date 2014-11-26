8741b2b74b3e8ef387fa4fbaf4b007b6efee576f
Sarup Banskota <sbanskota08@gmail.com>
2014-11-26 18:53:42 +0530

    Complete Array/List converters with helpers

--- data_structures/arrayList.js
+++ data_structures/arrayList.js
@@ -1,9 +1,21 @@
 // convert arrays to lists and vice-versa
 
+var prepend = function(element, list){
+	return { 
+		value: element, 
+		rest: list
+	};
+}
+
+var nth = function(list, number){
+	if(number){ return nth(list.rest, number-1); }
+	else{ return list.value; }
+}
+
 var arrayToList = function(array){
 	list = { value: array.pop(), rest: null };
 	while(array.length){
-		list = {value: array.pop(), rest: list};
+		list = prepend(array.pop(), list)
 	}
 	return list;
 }
@@ -17,6 +29,9 @@ var listToArray = function(list){
 	return array;
 }
 
-console.log(arrayToList([10, 20,30]));
-console.log(listToArray({ value: 10, rest: { value: 20, rest: { value: 30, rest: null }}}));
+console.log(arrayToList([10, 20]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
+
 
